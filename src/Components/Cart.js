9982a8cd50a1e403f507a348a0fe8b44347c85ac
Facundo Tejeda <facundotejeda18@gmail.com>
2022-07-19 20:21:17 -0300

    Incorporacion de Precio

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -3,24 +3,29 @@ import { cartContext } from "./CartContext";
 import { Link } from "react-router-dom";
 
 const Cart = () =>{
-    const {products,RemoveItem,Clear,QuantityProducts} = useContext(cartContext)
+    const {products,RemoveItem,Clear,QuantityProducts,Price} = useContext(cartContext)
 
 
 
-     return(<div  className="itemCartDetail">
-        
-    {QuantityProducts === 0 ? <h3>No hay producto en el Carrito</h3>:products.map((product) =>
-     <div key={product.id}>
-         <div className="card">   
-                 <h5 className="tittleCard">{product.title}</h5>
-                 <img src={product.pictureUrl} width="200px" height="200px"/>
-                 <Link to={`/item/${product.id}`}><button className="buttonCard">Ver Producto</button></Link>
-                 <p className="textCardCart">Precio: ${product.price}</p>
-                 <p className="textCardCart">Cantidad en Carrito: {product.qty}</p>
-                 <button onClick={()=>RemoveItem(product.id)}  className="buttonCardCart">Eliminar Item</button> <button onClick={Clear}  className="buttonCardCart">Limpiar Items</button>
-             </div>
-     </div>)}
-     
-     </div>)
+     return(
+      QuantityProducts === 0 ? <h3 className="tittle">No hay producto en el Carrito,  <Link to="/" className="toHome">Haga click aquí </Link> para agregar Productos.</h3>
+      :
+      <div>
+        <div  className="itemCartDetail">
+            {products.map((product) =>
+                <div key={product.id}>
+                    <div className="card">   
+                    <h5 className="tittleCard">{product.title}</h5>
+                    <img src={product.pictureUrl} width="200px" height="200px"/>
+                    <Link to={`/item/${product.id}`}><button className="buttonCard">Ver Producto</button></Link>
+                    <p className="textCardCart">Precio: ${product.price}</p>
+                    <p className="textCardCart">Cantidad en Carrito: {product.qty}</p>
+                    <button onClick={()=>RemoveItem(product.id)}  className="buttonCardCart">Eliminar Item</button> <button onClick={Clear}  className="buttonCardCart">Limpiar Items</button>
+                </div>
+        </div>)}
+        </div>
+        <h3 className="price">Precio Total de la Compra: ${Price}</h3>
+     </div>
+     )
 }
 export default Cart
--- src/Components/CartContext.js
+++ src/Components/CartContext.js
@@ -9,6 +9,8 @@ const CartContext= ({children}) =>{
     const [products, setProducts] = useState([])
 
     const [QuantityProducts,setQuantityProducts] = useState(0)
+    
+    const [Price,setPrice] = useState([0])
 
     const GetQuantityProducts = ()=>{
         let quantity = 0 
@@ -18,7 +20,6 @@ const CartContext= ({children}) =>{
         return (setQuantityProducts(quantity))
         
     }
-
     const AddItem = (product)=>{
         if(IsInCart(product.id)){
             const found = products.find(p=>p.id === product.id)
@@ -37,15 +38,22 @@ const CartContext= ({children}) =>{
     const Clear = ()=>{
         setProducts([])
         GetQuantityProducts(0)
+        PriceFinally(0)
     }
 
     const IsInCart= (id)=>{
         
         return (products.some(product=>product.id===id))
     }
-
+    const PriceFinally = ()=>{
+        let price = 0
+        products.forEach(product=>{
+            price += ((product.price)*(product.qty))})
+            return(setPrice(price))
+    }
     useEffect(()=>{
         GetQuantityProducts();
+        PriceFinally()
         console.log(products)
     },[products])
 
@@ -53,7 +61,7 @@ const CartContext= ({children}) =>{
 
 
     return(
-        <Provider value={{products, AddItem, RemoveItem, Clear, IsInCart, QuantityProducts}}>
+        <Provider value={{products, AddItem, RemoveItem, Clear, IsInCart, QuantityProducts, Price}}>
             {children}
         </Provider>
     )
