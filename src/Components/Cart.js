f5ca30d8a5e83797efbdd7d3d26df15875132b07
Facundo Tejeda <facundotejeda18@gmail.com>
2022-07-27 22:32:38 -0300

    Incorporacion de Formulario y Firebase

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -1,32 +1,11 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext } from "react";
 import { cartContext } from "./CartContext";
 import { Link } from "react-router-dom";
-import { doc,addDoc,collection,serverTimestamp } from "firebase/firestore";
-import { db } from "../Firebase/Firebase";
+
 
 const Cart = () =>{
     const {products,RemoveItem,Clear,QuantityProducts,Price} = useContext(cartContext)
-    const [idVenta,setIdventa]=useState("")
-    const datosComprador = {
-        nombre:"leo",
-        apellido:"lele",
-        correo:"leleleo@gmail.com"
-    }
-    const finalizarCompra=()=>{
-        const ventasCollection = collection(db,"ventas");
-        addDoc(ventasCollection, {
-            datosComprador,
-            items: products,
-            date:serverTimestamp(),
-            total:Price,
-            // stock:products.forEach(element => {
-            //     (element.stock) - (element.qty)               
-            // }),
-        }).then((result)=>
-            setIdventa(result.id),
-            console.log(idVenta)
-        )
-    }
+
 
      return(
       QuantityProducts === 0 ? <h3 className="tittle">¡El carrito esta vacío! <Link to="/" className="toHome"> Haga click aquí </Link> para agregar Productos.</h3>
@@ -45,10 +24,12 @@ const Cart = () =>{
                 </div>
         </div>)}
         </div>
-        <button onClick={Clear}  className="buttonCardCart">Limpiar Items</button>
-        <h3 className="price">Precio Total de la Compra: ${Price}</h3>
-        <Link to={"/form"}><button className="buttonCardCart">Finalizar Compra</button></Link>
-     </div>
+        <div className="cartCompra">
+            <button onClick={Clear}  className="buttonCardCart">Limpiar Items</button>
+            <h3 className="price">Precio Total de la Compra: ${Price}</h3>
+            <Link to={"/form"}><button className="buttonCardCart">Finalizar Compra</button></Link>
+        </div>
+        </div>
      )
 }
 export default Cart
--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,19 +1,56 @@
 import React from "react";
+import { useContext, useState } from "react";
+import { cartContext } from "./CartContext";
+import { addDoc,collection,serverTimestamp } from "firebase/firestore"
+import { db } from "../Firebase/Firebase";
+
 
 const Form = () =>{
-    
-return(
-    <form>
-        <label >Nombre
-        <input name="name" type="text"></input></label>
-        <label >Apellido
-        <input name="surname" type="text"></input></label>
-        <label>DNI
-        <input name="dni" type="number"></input></label>
-        <label>Tarjeta numero
-        <input name="tarjeta" type="number"></input></label>
-        <button type="submit">Finalizar</button>
+    const {products,Price,Clear} = useContext(cartContext)
+    const [buyer, setBuyer] = useState({});
+    const [idVenta,setIdVenta]=useState("")
+
+    const handleChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setBuyer(values => ({...values, [name]: value}))
+      }
+    const handleSubmit = (event) => {
+        event.preventDefault();
+      }
+
+    const finalizarCompra=()=>{
+        const ventasCollection = collection(db,"ventas");
+        addDoc(ventasCollection, {
+            buyer,
+            items: products,
+            date:serverTimestamp(),
+            total:Price,
+        }).then((result)=>{
+            return(
+                setIdVenta(result.id),
+                Clear()
+            )})
+    }
+
+return(<>
+    <form onSubmit={handleSubmit}>
+        <label className="textDetail" >Nombre</label><br />
+        <input onChange={handleChange} className="form" name="name" type="text" value={buyer.name}  required></input>  
+        <label className="textDetail">Apellido</label><br />
+        <input onChange={handleChange} className="form" name="surname" type="text" value={buyer.surname} required></input>
+        <label className="textDetail">DNI</label><br />
+        <input onChange={handleChange} className="form" name="dni" type="number" value={buyer.dni}  required></input>
+        <label className="textDetail">Celular</label><br />
+        <input onChange={handleChange} className="form" name="phone" type="number" value={buyer.phone}  required></input>
+        <label className="textDetail">Correo Electronico</label><br />
+        <input onChange={handleChange} className="form" name="mail" type="text" value={buyer.mail} required ></input> <br />
+        <button type="form-submit" className="agregarCarrito" onClick={finalizarCompra}>Finalizar</button>
     </form>
+        <div>
+            {idVenta ? <h3 className="tittle">Su predido se ha realizado con Exito, su solicitud fue guardad bajo el <span className="id">{idVenta}</span></h3> : <h3>Para confirmar su pedido presione Finalizar</h3>}
+        </div>
+    </>
     )
 }
 
